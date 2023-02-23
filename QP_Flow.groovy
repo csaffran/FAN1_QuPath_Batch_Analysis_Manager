1b22e3955180e37992e95c29140a21fd9ba826a4
csaffran <46048814+csaffran@users.noreply.github.com>
2023-02-23 18:42:45 -0500

    Update QP_Flow.groovy

--- QP_Flow.groovy
+++ QP_Flow.groovy
@@ -1,6 +1,6 @@
 setImageType('BRIGHTFIELD_H_E');
 setColorDeconvolutionStains('{"Name" : "H&E default", "Stain 1" : "Hematoxylin", "Values 1" : "0.65111 0.70119 0.29049", "Stain 2" : "Eosin", "Values 2" : "0.2159 0.8012 0.5581", "Background" : " 255 255 255"}');
 selectAnnotations();
-saveAnnotationMeasurements('/Users/FULL DIRECTORY PATH WHERE YOU WANT FILES SAVED/')
 runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{"detectionImageBrightfield": "Hematoxylin OD",  "requestedPixelSizeMicrons": 0.2431,  "backgroundRadiusMicrons": 10.0,  "medianRadiusMicrons": 0.0,  "sigmaMicrons": 1.5,  "minAreaMicrons": 10.0,  "maxAreaMicrons": 400.0,  "threshold": 0.1,  "maxBackground": 2.0,  "watershedPostProcess": true,  "cellExpansionMicrons": 5.0,  "includeNuclei": true,  "smoothBoundaries": true,  "makeMeasurements": true}');
+saveAnnotationMeasurements('/Users/FULL DIRECTORY PATH WHERE YOU WANT FILES SAVED/')
 saveDetectionMeasurements('/Users/FULL DIRECTORY PATH WHERE YOU WANT FILES SAVED/')
